6fe567e58d3cecdc92a6349abe1a3ef1c28b65e1
peterh3g <peterh3g@gmail.com>
2022-05-03 18:49:47 +0200

    add gh-pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@ import { defineNuxtConfig } from "nuxt";
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   buildModules: ["@unocss/nuxt"],
+  mode: 'spa',
+  router: {
+    base: '/'
+  },
   runtimeConfig: {
     public: {
       appName: "",
