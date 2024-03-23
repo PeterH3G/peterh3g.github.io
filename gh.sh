3e47ccd654237e601d8bf7423372fe0903fd9492
PeterH3G <peterh3g@gmail.com>
2024-03-23 23:11:41 +0100

    Ready for deployment

--- gh.sh
+++ gh.sh
@@ -5,4 +5,4 @@ git add .
 git commit -m "Ready for deployment"
 git remote add origin https://github.com/PeterH3G/peterh3g.github.io.git
 git branch -M main
-git push -u origin --all
+git push -u origin --force
