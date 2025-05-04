8a2b2b945db59b10ea48fbab0835a10c38b1141a
peterh3g <peterh3g@gmail.com>
2025-05-04 22:18:52 +0200

    Project setup

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -3,11 +3,16 @@ const route = useRoute()
 const { data: page } = await useAsyncData(route.path, () => {
   return queryCollection('content').path(route.path).first()
 })
+
+useSeoMeta({
+  title: page.value?.title,
+  description: page.value?.description
+})
 </script>
 
 <template>
   <UCard>
-    <!-- Render the blog post as Prose & Vue components -->
     <ContentRenderer v-if="page" :value="page" />
+    <div v-else>Content not found</div>
   </UCard>
 </template>
--- content.config.ts
+++ content.config.ts
@@ -4,17 +4,15 @@ export default defineContentConfig({
   collections: {
     content: defineCollection({
       // Load every file inside the `content` directory
-      source: '**',
+      source: '**/*.md',
       
       // Specify the type of content in this collection
       type: 'page',
 
-      // Define custom schema for docs collection
+      // Define custom schema for collection
       schema: z.object({
         tags: z.array(z.string()),
-        icon: z.string(),
-        image: z.string(),
-        date: z.date()
+        icon: z.string()
       })
     })
   }
