1e36e80d8cc1e06ffa3031a530de151f11523f96
peterh3g <peterh3g@gmail.com>
2025-05-05 21:17:29 +0200

    Project setup

--- app/components/app/footer.vue
+++ app/components/app/footer.vue
@@ -31,36 +31,27 @@ const footer = computed(() => {
 
 <template>
   <UContainer as="footer" class="app-footer text-xs xs:flex xs:flex-col md:flex items-center justify-between gap-4">
-    <UCard as="section" :ui="{
-      root: 'footer-github flex flex-col justify-center items-center w-full'
-    }">
+    <section class="footer-github w-full mb-4 md:mb-0">
       <UButton v-for="item in footer.items" :key="item.url" trailing-icon="mdi:open-in-new" :href="item.url"
         target="_blank" size="sm" :variant="item.variant" :ui="{
-          base: 'flex'
+          base: 'w-full h-full justify-center items-center'
         }">
         <UIcon :name="item.icon" />
         <span v-text="item.title" />
       </UButton>
-    </UCard>
+    </section>
 
-    <UCard as="section" :ui="{
-      root: 'footer-disclaimer flex items-center justify-center text-center w-full'
-    }">
+    <section class="footer-disclaimer flex flex-col items-center justify-center w-full h-full">
+      <strong class="flex items-center justify-center">
+        <NuxtImg :src="footer.avatarUrl" alt="logo" width="18" height="18" />
+        <span v-text="footer.name" />
+      </strong>
 
-      <div class="app-title flex flex-col w-full">
-        <strong class="flex items-center justify-center">
-          <NuxtImg :src="footer.avatarUrl" alt="logo" width="18" height="18" />
-          <span v-text="footer.name" />
-        </strong>
-        <em v-text="footer.description" />
-      </div>
-
-      <div class="app-disclaimer flex md:flex-col items-center justify-center w-full">
-        <span class="inline-flex items-center">
-          <Icon name="mdi:copyright" />
-          2025 All rights reserved
-        </span>
-      </div>
-    </UCard>
+      <em v-text="footer.description" />
+      <span class="inline-flex items-center">
+        <Icon name="mdi:copyright" />
+        2025 All rights reserved
+      </span>
+    </section>
   </UContainer>
 </template>
