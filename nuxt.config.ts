bd311f2ebe4eab8cf96187fd26d13a9aaa60c548
peterh3g <peterh3g@gmail.com>
2025-05-05 15:28:56 +0200

    Project setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -51,7 +51,7 @@ export default defineNuxtConfig({
   // https://icon-sets.iconify.design/mdi/?keyword=mdi
   icon: {
     serverBundle: {
-      collections: ['mdi']
+      collections: ['local']
     }
   },
 })
