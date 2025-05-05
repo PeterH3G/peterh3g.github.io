de1a302fe5af32d62b2f96ed020077f6703aca25
peterh3g <peterh3g@gmail.com>
2025-05-05 20:53:08 +0200

    Project setup

--- app/components/app/footer.vue
+++ app/components/app/footer.vue
@@ -17,7 +17,7 @@ const footer = computed(() => {
     {
       icon: 'mdi:github' as string,
       url: 'https://github.com/PeterH3G/peterh3g.github.io',
-      title: 'Application Repository',
+      title: 'Github Repository',
       variant: 'ghost'
     }
   ]
@@ -30,40 +30,37 @@ const footer = computed(() => {
 </script>
 
 <template>
-  <UContainer as="footer" class="app-footer text-xs">
-    <div class="footer-github">
+  <UContainer as="footer" class="app-footer text-xs xs:flex xs:flex-col md:flex items-center justify-between gap-4">
+    <UCard as="section" :ui="{
+      root: 'footer-github flex flex-col justify-center items-center w-full'
+    }">
       <UButton v-for="item in footer.items" :key="item.url" trailing-icon="mdi:open-in-new" :href="item.url"
-        target="_blank" size="sm" :variant="item.variant">
+        target="_blank" size="sm" :variant="item.variant" :ui="{
+          base: 'flex'
+        }">
         <UIcon :name="item.icon" />
         <span v-text="item.title" />
       </UButton>
-    </div>
+    </UCard>
 
-    <div class="footer-disclaimer">
-      <NuxtImg :src="footer.avatarUrl" alt="logo" width="18" height="18" class="m-auto" />
-      <span v-text="footer.name" />
-      <span v-text="footer.description" />
-      <span>&copy;2025 | All rights reserved.</span>
-    </div>
+    <UCard as="section" :ui="{
+      root: 'footer-disclaimer flex items-center justify-center text-center w-full'
+    }">
+
+      <div class="app-title flex flex-col w-full">
+        <strong class="flex items-center justify-center">
+          <NuxtImg :src="footer.avatarUrl" alt="logo" width="18" height="18" />
+          <span v-text="footer.name" />
+        </strong>
+        <em v-text="footer.description" />
+      </div>
+
+      <div class="app-disclaimer flex md:flex-col items-center justify-center w-full">
+        <span class="inline-flex items-center">
+          <Icon name="mdi:copyright" />
+          2025 All rights reserved
+        </span>
+      </div>
+    </UCard>
   </UContainer>
 </template>
-
-<style lang="css" scoped>
-.app-footer {
-  @apply grid grid-cols-[.3fr_.4fr_.3fr] grid-rows-1;
-  @apply items-center justify-center;
-  grid-template-areas: "Github Disclaimer ..."
-}
-
-.footer-github {
-  @apply grid grid-cols-1 grid-rows-[auto] h-full;
-  grid-area: Github;
-}
-
-.footer-disclaimer {
-  @apply grid grid-cols-1 grid-rows-3;
-  @apply items-center justify-center;
-  grid-area: Disclaimer;
-  text-align: center;
-}
-</style>
