e05b134230d7e67968676b46b9f113702bbe983d
peterh3g <peterh3g@gmail.com>
2025-05-05 12:19:31 +0200

    Project setup

--- app/components/button/appTheme.vue
+++ app/components/button/appTheme.vue
@@ -17,14 +17,12 @@ const isDark = computed({
 })
 
 const label = props.label || 'app-theme'
-const iconLight = 'mdi:weather-sunny'
-const iconDark = 'mdi:weather-night'
 const variant = props.variant || 'ghost'
 </script>
 
 <template>
     <ClientOnly v-if="!colorMode?.forced">
-        <UButton class="app-theme" :icon="isDark ? `${iconLight}` : `${iconDark}`" :label="label"
+        <UButton class="app-theme" :icon="isDark ? `mdi:weather-sunny` : `mdi:weather-night`" :label="label"
             :variant="variant" @click="isDark = !isDark" :ui="{
                 base: props.class,
             }" />
