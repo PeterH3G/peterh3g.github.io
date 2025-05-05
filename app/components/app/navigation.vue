e3aa8bef32f7607214b033e08adeaff8ba609163
peterh3g <peterh3g@gmail.com>
2025-05-05 18:15:25 +0200

    Project setup

--- app/components/app/navigation.vue
+++ app/components/app/navigation.vue
@@ -4,22 +4,21 @@ const props = defineProps<{
 }>()
 
 const { data: items } = await useAsyncData('navigation', async () => {
-    return (await queryCollectionNavigation('content', ['icon', 'title', 'path'])).map((item) => ({
+    return (await queryCollectionNavigation('content', ['icon', 'title', 'description', 'path'])).map((item) => ({
         label: item.title,
         icon: item.icon as string, // Type assertion for 'icon' to be a string
-        to: item.path
+        to: item.path,
+        description: item.description as string, // Type assertion for 'description' to be a string
     }))
 })
 </script>
 
 <template>
     <UNavigationMenu highlight highlight-color="primary" :orientation="props.isHeader ? 'horizontal' : 'vertical'"
-        :items="items" class="data-[orientation=horizontal]:horizontal data-[orientation=vertical]:vertical"
-        :class="isHeader ? 'hidden md:flex' : ''" />
+        :items="items" class="data-[orientation=horizontal]:horizontal data-[orientation=vertical]:vertical" :ui="{
+            root: `${isHeader ? 'hidden md:flex' : 'w-full h-full'}`,
+            list: isHeader ? '' : 'w-full h-full',
+            item: isHeader ? '' : 'w-full h-full',
+            link: isHeader ? '' : 'w-full h-full text-2xl'
+        }" />
 </template>
-
-<style lang="css" scoped>
-.horizontal {
-    @apply justify-end w-full;
-}
-</style>
