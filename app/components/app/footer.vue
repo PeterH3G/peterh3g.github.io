e81db1112f243ae7a22cea25c28c0439d49d058a
peterh3g <peterh3g@gmail.com>
2025-05-05 14:32:46 +0200

    Project setup

--- app/components/app/footer.vue
+++ app/components/app/footer.vue
@@ -4,17 +4,51 @@ const props = defineProps<{
 }>()
 
 const footer = computed(() => {
-  return props.footer
+  const socials = [
+    {
+      icon: 'mdi:github' as string,
+      url: 'https://github.com/peterh3g',
+      title: 'My Github Profile',
+      variant: 'ghost'
+    },
+    {
+      icon: 'mdi:github' as string,
+      url: 'https://github.com/PeterH3G/peterh3g.github.io',
+      title: 'Application Repository',
+      variant: 'ghost'
+    }
+  ]
+
+  return {
+    ...props.footer,
+    socials
+  }
 })
 </script>
 
 <template>
   <UContainer as="footer" class="app-footer">
-    <div class="flex items-center justify-center">
+    <div class="footer-socials">
+      <UButton v-for="item in footer.socials" :key="item.url" trailing-icon="mdi:open-in-new" :href="item.url"
+        target="_blank" size="sm" :variant="item.variant">
+        <UIcon :name="item.icon" />
+        <span v-text="item.title" />
+      </UButton>
+    </div>
+
+    <div class="footer-bottom text-xs">
       <ButtonAppLogo :logo="{ type: 'default', avatarUrl: footer.avatarUrl, name: footer.name }" />
-      <p>&copy;2025 | All rights reserved.</p>
+      <span class="pr-1">&copy;2025 | All rights reserved.</span>
     </div>
   </UContainer>
 </template>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+.footer-socials {
+  @apply flex flex-col items-center;
+}
+
+.footer-bottom {
+  @apply flex items-center justify-center;
+}
+</style>
